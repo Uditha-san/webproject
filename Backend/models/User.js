4715b7416db0abc732105156ba19df4ee65da93b
kavindusa141 <ksandeepa1428@gmail.com>
2025-08-01 22:02:45 +0530

    models for mongoDB

---
+++ Backend/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: { type: String, required: true ,select: false},
+    role: {type: String, enum: ["user", "hotelOwner"], default: "user"},
+    recentSearchedCities: [{type: String, required: true}],
+
+
+    // 🔒 New fields for email verification
+  isVerified: { type: Boolean, default: false },
+  emailVerificationToken: { type: String },
+  emailVerificationExpires: { type: Date }
+  
+}, {timestamps: true});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+
+export default User;
