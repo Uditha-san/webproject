9d03ea31b8e6bf2261e9ef31e6cc86412a2ff971
Uditha Sandeepa <udithasandeepa8@gmail.com>
2025-08-02 12:25:49 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -42,82 +42,143 @@ const Navbar = ({ onLoginClick, onRegisterClick }) => {
     }, [location.pathname]);
 
     return (
-            <nav className={`fixed top-0 left-0  w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/30 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
+      <nav
+        className={`fixed top-0 left-0  w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${
+          isScrolled
+            ? "bg-white/30 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4"
+            : "py-4 md:py-6"
+        }`}
+      >
+        {/* Logo */}
+        <Link to="/">
+          <img
+            src={assets.logo}
+            alt="logo"
+            className={`h-9 ${isScrolled && "invert opacity-80"}`}
+          />
+        </Link>
 
-                {/* Logo */}
-                <Link to ='/'>
-                    <img src={assets.logo} alt="logo" className={`h-9 ${isScrolled && "invert opacity-80"}`} />
-                </Link>
+        {/* Desktop Nav */}
+        <div className="hidden md:flex items-center gap-4 lg:gap-8">
+          {navLinks.map((link, i) => (
+            <Link
+              key={i}
+              to={link.path}
+              className={`group flex flex-col gap-0.5 ${
+                isScrolled ? "text-gray-700" : "text-white"
+              }`}
+            >
+              {link.name}
+              <div
+                className={`${
+                  isScrolled ? "bg-gray-700" : "bg-white"
+                } h-0.5 w-0 group-hover:w-full transition-all duration-300`}
+              />
+            </Link>
+          ))}
+          <button
+            className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
+              isScrolled ? "text-black" : "text-white"
+            } transition-all`}
+            onClick={() => navigate("/dashboard")}
+          >
+            Dashboard
+          </button>
+        </div>
 
-                {/* Desktop Nav */}
-                <div className="hidden md:flex items-center gap-4 lg:gap-8">
-                    {navLinks.map((link, i) => (
-                        <Link key={i} to={link.path} className={`group flex flex-col gap-0.5 ${isScrolled ? "text-gray-700" : "text-white"}`}>
-                            {link.name}
-                            <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
-                        </Link>
-                    ))}
-                    <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}
-                    onClick={()=> navigate('/dashboard')}>
-                        Dashboard
-                    </button>
-                </div>
+        {/* Desktop Right */}
+        <div className="hidden md:flex items-center gap-4">
+          <img
+            src={assets.searchIcon}
+            alt="search"
+            className={` ${
+              isScrolled && "invert"
+            } h-7 transition-all duration-500`}
+          />
 
-                {/* Desktop Right */}
-                <div className="hidden md:flex items-center gap-4">
-                    <img src={assets.searchIcon} alt="search" className={` ${isScrolled && "invert"} h-7 transition-all duration-500`} />
+          {user ? (
+            <div className="flex items-center gap-2">
+              <button
+                onClick={() => navigate("/my-bookings")}
+                className="flex items-center gap-1 text-sm"
+              >
+                <BookIcon />
+                My Bookings
+              </button>
+              <button onClick={logout} className="text-sm">
+                Logout
+              </button>
+            </div>
+          ) : (
+            <button
+              onClick={onLoginClick}
+              className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${
+                isScrolled ? "text-white bg-black" : "bg-white text-black"
+              }`}
+            >
+              Login
+            </button>
+          )}
+        </div>
 
-                    {user ? 
-                    (<div className="flex items-center gap-2">
-                        <button onClick={() => navigate('/my-bookings')} className="flex items-center gap-1 text-sm">
-                            <BookIcon />
-                            My Bookings
-                        </button>
-                        <button onClick={logout} className="text-sm">Logout</button>
-                    </div>)
-                :
-                (<button onClick={onLoginClick} className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
-                        Login
-                    </button>)}
+        {/* Mobile Menu Button */}
 
-                    
-                </div>
+        <div className="flex items-center gap-3 md:hidden">
+          {user && (
+            <button
+              onClick={() => navigate("/my-bookings")}
+              className="flex items-center gap-1 text-sm"
+            >
+              <BookIcon />
+              My Bookings
+            </button>
+          )}
+          <img
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            src={assets.menuIcon}
+            alt="menu"
+            className={` ${isScrolled && "invert"} h-4`}
+          />
+        </div>
 
-                {/* Mobile Menu Button */}
+        {/* Mobile Menu */}
+        <div
+          className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${
+            isMenuOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <button
+            className="absolute top-4 right-4"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            <img src={assets.closeIcon} alt="close" className="h-6.5" />
+          </button>
 
-                
-                <div className="flex items-center gap-3 md:hidden">
+          {navLinks.map((link, i) => (
+            <Link key={i} to={link.path} onClick={() => setIsMenuOpen(false)}>
+              {link.name}
+            </Link>
+          ))}
 
-                    {user && <button onClick={() => navigate('/my-bookings')} className="flex items-center gap-1 text-sm">
-                        <BookIcon />
-                        My Bookings
-                    </button>}
-                    <img onClick={() => setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="menu" className={` ${isScrolled && "invert"} h-4`} />
-                </div>
+          {user && (
+            <button
+              className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all"
+              onClick={() => navigate("/dashboard")}
+            >
+              Dashboard
+            </button>
+          )}
 
-                {/* Mobile Menu */}
-                <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                    <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-                        <img src={assets.closeIcon} alt="close" className="h-6.5" />
-                    </button>
-
-                    {navLinks.map((link, i) => (
-                        <Link key={i} to={link.path} onClick={() => setIsMenuOpen(false)}>
-                            {link.name}
-                        </Link>
-                    ))}
-
-                    {user && <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all"
-                    onClick={()=> navigate('/dashboard')}>
-                        Dashboard
-                    </button>}
-
-                    {!user && <button onClick={onLoginClick} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
-                        Login
-                    </button>}
-                </div>
-            </nav>
-       
+          {!user && (
+            <button
+              onClick={onLoginClick}
+              className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500"
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </nav>
     );
 }
 
